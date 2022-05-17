a413a46bb575b0978bcffbe783073ee7e22e0139
Bich.Phuong <bichphuonggb@gmail.com>
2022-05-17 09:29:33 +0800

    heavy_stone

---
+++ app/Models/Api/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\Api;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id_user',
+        'theme',
+        'content',
+        'status'
+    ];
+}
